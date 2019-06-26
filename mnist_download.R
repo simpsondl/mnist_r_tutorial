8d5e149b509a92057441f9a73ae14bc171962732
simpsondl <49914433+simpsondl@users.noreply.github.com>
2019-06-26 12:52:02 -0400

    Update mnist_download.R

--- mnist_download.R
+++ mnist_download.R
@@ -1,9 +1,9 @@
 # Package requirements
 pkgs <- c("keras","reshape2","ggplot2")
 
-# Check if packages already installed and install if not
+# Check if packages already installed, stop script and alert user if not
 new.pkgs <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
-if(length(new.pkgs)) install.packages(new.pkgs)
+if(length(new.pkgs)) stop(paste("Missing packages", new.pkgs,"\n"))
 
 # Load keras and save the mnist dataset
 library(keras)
